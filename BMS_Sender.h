4783a87a2cd9d2d1279d74bcdb5642b442411048
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-02-16 19:30:18 +0530

    Update BMS sender to integrate sensor sample formatter and IO

--- BMS_Sender.h
+++ BMS_Sender.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int BMS_SendSensorData();
+typedef void (*bmsIoFuncPtr)(char *, int);
+
 void streamBmsSensorData();
+void setStreamIoFunc(bmsIoFuncPtr funcPtr);
