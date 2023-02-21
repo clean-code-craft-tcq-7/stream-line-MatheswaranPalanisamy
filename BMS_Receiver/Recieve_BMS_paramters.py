72278c41a9c4912b40f58b77471e2d992b07de98
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-02-21 21:20:58 +0530

    updating the Receive_BMS_parameters.py for limiting complexity and updating the workflow

--- BMS_Receiver/Recieve_BMS_paramters.py
+++ BMS_Receiver/Recieve_BMS_paramters.py
@@ -7,20 +7,15 @@ Created on Tue Feb 21 11:30:02 2023
 
 import csv
 
-def check_validity_of_parameter_values(value):
-    if len(parameter_values) > 1:
-        return True
-    return False
 
 def receive_bms_paramters(sender_input):
     charge_rate = []
     temperature = []
     
     bms_parameters = csv.reader(sender_input.split('\n'), delimiter=',')
+    bms_parameters = [value for value in bms_parameters][1:-1]
     
     for parameter_values in bms_parameters:
-        if(check_validity_of_parameter_values(parameter_values)):
-            if parameter_values[0] != 'ChargeRate':
-                charge_rate.append(float(parameter_values[0]))
-                temperature.append(float(parameter_values[1]))
+        charge_rate.append(float(parameter_values[0]))
+        temperature.append(float(parameter_values[1]))
     return (charge_rate, temperature)
